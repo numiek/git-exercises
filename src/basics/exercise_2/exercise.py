b8b289b3c1450aceb8b4e92f6399833ee72fd169
Nick <schmitz-nick@outlook.de>
2024-03-01 23:24:18 +0100

    reworded

--- src/basics/exercise_2/exercise.py
+++ src/basics/exercise_2/exercise.py
@@ -1,6 +1,6 @@
 """
 Problem Description:
-    Implement a function copy_git that returns the required command to copy a git repository from https://example.com to /subdir, 
+    Implement a function 'copy_git' that returns the required command to copy a git repository from 'https://example.com' to '/subdir', 
     which is located inside the current working directory.
 """
 
