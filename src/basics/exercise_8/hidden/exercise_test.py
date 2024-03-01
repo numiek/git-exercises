4f31233a30875158be921d63f8c4b2802a05412c
Nick <schmitz-nick@outlook.de>
2024-03-01 23:28:58 +0100

    reworked exercise

--- src/basics/exercise_8/hidden/exercise_test.py
+++ src/basics/exercise_8/hidden/exercise_test.py
@@ -6,7 +6,7 @@ DIR_PATH = pathlib.Path(__file__).parent.parent
 
 def test_gitignore_specific_files():
     with open(DIR_PATH / ".gitignore", "r") as f:
-        lines = f.read().splitlines()
+        lines = f.read().strip()
 
-    assert "secrets.txt" in lines, "Failed to ignore 'secrets.txt'"
-    assert "config.txt" not in lines, "'config.txt' should not be ignored"
+    assert "/secrets.txt" in lines
+    assert "config.txt" not in lines
