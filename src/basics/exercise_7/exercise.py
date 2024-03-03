fd5e7efc632cc88d37cd2ac0c70107665a983751
Nick <schmitz-nick@outlook.de>
2024-03-03 20:31:01 +0100

    adapted problem statement

--- src/basics/exercise_7/exercise.py
+++ src/basics/exercise_7/exercise.py
@@ -1,5 +1,5 @@
 """
 Problem Description:
-    Manually create a .gitignore file that ignores all '.log' files and the 'temp/' directory, which are inside this directory.
+    Manually create a .gitignore file that ignores all '.log' files and the 'temp/' directory, which are both inside this directory.
     Additionally, any 'invalid/' directory must be ignored.
 """
