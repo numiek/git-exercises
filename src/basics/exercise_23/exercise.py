f182f15d81d6025ebf3672b42ce732ed99ba52d3
Nick Schmitz <schmitz-nick@outlook.de>
2024-03-12 18:28:16 +0100

    solved exercise

--- src/basics/exercise_23/exercise.py
+++
@@ -1,8 +0,0 @@
-"""
-Problem Description:
-    Does 'git rm' support plain glob patterns? Return True or False.
-"""
-
-
-def answer() -> bool:
-    return None
--- src/basics/exercise_23/hidden/exercise_test.py
+++
@@ -1,5 +0,0 @@
-from src.basics.exercise_23.exercise import answer
-
-
-def test_answer():
-    assert answer()
