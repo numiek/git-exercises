9b21e1d0e63a7ccf963debe49881157130277ae3
Nick Schmitz <schmitz-nick@outlook.de>
2024-03-12 18:14:44 +0100

    fixed word repetition

--- src/basics/exercise_18/exercise.py
+++ src/basics/exercise_18/exercise.py
@@ -1,6 +1,6 @@
 """
 Problem Description:
-    Before you push your commit to the the remote repository, you want to perform a small adjustment and include it
+    Before you push your commit to the remote repository, you want to perform a small adjustment and include it
     in your last commit. You want the message to be "small adjustment".
 
     Return the required Git commands that must be executed in succession.
