41bab08b9272bfe01dcf2731f84f99a18b20c437
Nick Schmitz <schmitz-nick@outlook.de>
2024-03-24 18:44:26 +0100

    reworded problem statement

--- src/basics/exercise_1/exercise.py
+++ src/basics/exercise_1/exercise.py
@@ -1,6 +1,6 @@
 """
 Problem Description:
-    Implement a function initialize_git that returns the required command to transform a directory named "subdir", 
+    Implement a function `initialize_git` that returns the required command to transform a directory named "subdir", 
     which is located inside the current working directory, into a Git repository.
 """
 
